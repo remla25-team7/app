9a8926ca8c937fc8ab9a5144eecfb2ef49f1a3ff
Razvan Loghin <R.Loghin@student.tudelft.nl>
2025-06-22 19:51:49 +0200

    Added Swager and versioning in the frontend

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flasgger import Swagger
 from app.routes import bp
 
 def create_app():
     app = Flask(__name__, template_folder="templates")
+    swagger = Swagger(app)
     app.register_blueprint(bp)
     return app
